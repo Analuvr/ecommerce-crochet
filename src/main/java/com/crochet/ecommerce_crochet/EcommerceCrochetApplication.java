d686f700f3e0a1217e2e3d01036bab96b0a141af
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-08-26 09:10:08 -0600

    Proyecto inicial Spring Boot con Maven y MySQL

---
+++ src/main/java/com/crochet/ecommerce_crochet/EcommerceCrochetApplication.java
@@ -0,0 +1,13 @@
+package com.crochet.ecommerce_crochet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCrochetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCrochetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crochet/ecommerce_crochet/EcommerceCrochetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crochet.ecommerce_crochet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCrochetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
